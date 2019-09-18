efeb87cb484341a7d2594134296ba689a69090bc
JackChen <clanzl@126.com>
2019-09-18 08:56:29 +0800

    提交Sqlsugar测试代码
    Oracle Clob字段映射出现错误

---
+++ Dbtestclob.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using SqlSugar;
+
+namespace TestClobOrm
+{
+    ///<summary>
+    ///
+    ///</summary>
+    [SugarTable("DBTESTCLOB")]
+    public partial class Dbtestclob
+    {
+           public Dbtestclob(){
+
+
+           }
+           /// <summary>
+           /// Desc:
+           /// Default:
+           /// Nullable:False
+           /// </summary>           
+           [SugarColumn(IsPrimaryKey=true)]
+           public string ID {get;set;}
+
+           /// <summary>
+           /// Desc:
+           /// Default:
+           /// Nullable:True
+           /// </summary>           
+           public string DATA {get;set;}
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SqlSugar;
+using DbType = System.Data.DbType;
+
+namespace TestClobOrm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connectStr =
+                string.Format(
+                    "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST={0})(PORT={1})))(CONNECT_DATA=(SERVICE_NAME={2})));User Id={3};Password={4};",
+                    "192.168.10.60", "1521", "his", "emr", "test");
+            SqlSugarClient db = new SqlSugarClient(new ConnectionConfig()
+            { 
+                ConnectionString = connectStr,
+                DbType = SqlSugar.DbType.Oracle,        　//必填, 数据库类型
+                IsAutoCloseConnection = true,       //默认false, 时候知道关闭数据库连接, 设置为true无需使用using或者Close操作
+                InitKeyType = InitKeyType.SystemTable    //默认SystemTable, 字段信息读取, 如：该属性是不是主键，是不是标识列等等信息
+            });
+
+          var res = db.Queryable<Dbtestclob>().ToList();
+        }
+    }
+}
